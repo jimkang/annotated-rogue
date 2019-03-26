a5253427b99eb8d9e6f9285577b60f67b66a37b0
Jim Kang <jimkang@gmail.com>
2019-03-25 21:45:38 -0400

    Smaller code blocks.

--- marked-up-src/linuxrogue-0.3.7/main.c
+++ marked-up-src/linuxrogue-0.3.7/main.c
@@ -48,9 +48,9 @@ int main(int argc, char *argv[])
 		goto PL;//||{"next": {"line": 62}}
 	}
 
+  //||block||{"note": "We are in main(), the first function that runs in all C programs. Rogue uses its main() to do some housekeeping and then set up its main outer loop, which sets up a level and everything that goes with it, lets the player play that level (another loop)."}
 	for (;;)
 	{
-    //||block||{"note": "Make all the things that go in the map."}
 		clear_level();
 		make_level();
 		put_objects();
@@ -59,9 +59,9 @@ int main(int argc, char *argv[])
 		put_mons();
 		put_player(party_room);
 		print_stats(STAT_ALL);
-    //||block
 PL:		
-		play_level(); //||{"expand": {"file": "play.c", "lines": [152, 406]}, "note": "Now, allow them all to interact."}
+		play_level(); //||{"expand": {"file": "play.c", "lines": [152, 406]}, "note": "This is the inner loop that handles interaction with the level."}
+    //||block
 		free_stuff(&level_objects);
 		free_stuff(&level_monsters);
 	}//||{"next": {"line": 51}}
